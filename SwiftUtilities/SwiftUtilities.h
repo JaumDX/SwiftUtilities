513ed0f7c43ffa724976a7389531940bbd0c80db
Joao <joaovitornaru@hotmail.com>
2017-06-02 15:18:26 -0300

    Initial Commit

---
+++ SwiftUtilities/SwiftUtilities.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtilities.h
+//  SwiftUtilities
+//
+//  Created by Solid Jaum on 02/06/17.
+//  Copyright © 2017 Time dos Sonhos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtilities.
+FOUNDATION_EXPORT double SwiftUtilitiesVersionNumber;
+
+//! Project version string for SwiftUtilities.
+FOUNDATION_EXPORT const unsigned char SwiftUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtilities/PublicHeader.h>
+
+
